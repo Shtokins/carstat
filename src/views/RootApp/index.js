8bbc8d830772605a8b7b2bfa618a3f37afc36eba
Victor Volkov <volkovv71@gmail.com>
2020-07-03 17:21:18 +0300

    Sider: refactor

--- src/views/RootApp/index.js
+++ src/views/RootApp/index.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Layout, Menu } from "antd";
 import { MainChartBlock } from "../MainChartBlock";
 import { UIContext } from "../../context/ui/UIContext";
+import { UISider } from "./UISider";
 
 const { Sider } = Layout;
 
@@ -13,65 +14,15 @@ export const RootApp = () => {
   const { setUI, uiName } = useContext(UIContext);
 
   return (
-    <Layout style={{ minHeight: "100vh", backgroundColor: "#f2f2f2" }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={setCollapsed}
-        className="antd-sider"
-      >
-        <div className="logo">Logo</div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          selectedKeys={[uiName]}
-          onClick={setUI}
-        >
-          <Menu.Item
-            key="antd"
-            icon={
-              <img
-                alt=""
-                src={publicImageFolder + "/antdLogo.png"}
-                width={30}
-              />
-            }
-          >
-            Ant Design
-          </Menu.Item>
-          <Menu.Item
-            key="semantic"
-            icon={
-              <img alt="" src={publicImageFolder + "/suiLogo.png"} width={30} />
-            }
-          >
-            Semantic UI
-          </Menu.Item>
-          <Menu.Item
-            key="material"
-            icon={
-              <img alt="" src={publicImageFolder + "/muiLogo.png"} width={30} />
-            }
-          >
-            Material UI
-          </Menu.Item>
-          <Menu.Item
-            key="atlassian"
-            icon={
-              <img
-                alt=""
-                src={publicImageFolder + "/atlassianKitLogo.png"}
-                width={30}
-              />
-            }
-          >
-            Atlassian Kit
-          </Menu.Item>
-        </Menu>
-      </Sider>
+    <UISider
+      collapsed={collapsed}
+      setCollapsed={setCollapsed}
+      uiName={uiName}
+      setUI={setUI}
+    >
       <div className="dashboard container">
         <MainChartBlock />
       </div>
-    </Layout>
+    </UISider>
   );
 };
